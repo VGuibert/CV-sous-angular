311fa6573ea7b5a5e5cc63e7758bbc09f9cbfe73
Vguibert <vincent.guibert79@gmail.com>
2018-12-02 07:22:01 +0100

    Avancer controller et multipage

---
+++ Controllers/MainViewControllers.js
@@ -0,0 +1,18 @@
+angular.module('myCtrl', []).controller('CommonCtrl', function($scope) {
+
+  $scope.showMe = false;
+  $scope.showFunction = function() {
+    $scope.showMe = !$scope.showMe;
+  };
+
+  $scope.showMe2 = false;
+  $scope.showFunction2 = function() {
+    $scope.showMe2 = !$scope.showMe2;
+  };
+
+  $scope.showMe3 = false;
+  $scope.showFunction3 = function() {
+    $scope.showMe3 = !$scope.showMe3;
+  };
+
+});
